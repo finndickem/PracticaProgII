f34820f8059db5959cf970e597dfa409441694ed
finndickem <118976342+finndickem@users.noreply.github.com>
2024-03-21 17:38:45 +0100

    Add files via upload

---
+++ Tetris/src/tetris/Acciones.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tetris;
+
+import java.awt.event.*;
+
+/**
+ *
+ * @author Finn
+ */
+public class Acciones {
+    
+    
+    ActionListener Salir = e -> {
+            System.exit(0);
+        };
+    
+    
+}
